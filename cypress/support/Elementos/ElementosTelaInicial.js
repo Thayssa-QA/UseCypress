509152eb18cb6fa4d63c6a18e1971b68d00a7168
Thayssa <Thayssasilva3301@gmail.com>
2025-08-17 01:26:36 -0300

    Adicionando elementos da tela inicial

---
+++ cypress/support/Elementos/ElementosTelaInicial.js
@@ -0,0 +1,11 @@
+export const ElementosTelaInicial = {
+    logoDetran: "img[alt='DETRAN-PE']",
+    logoPE: ".logope",
+    carrosel: ".carousel-item img",
+    menubar: ".main-menu-link",
+    cardIpva: ".push-notification-feedback",
+    cardIpvaTable: ".push-notification-feedback table tbody tr",
+    linkCalendarioIpva: ".link-dtl-servico",
+    btnCookies: ".btn-cookie",
+    areaAcessibilidade: "#acessibilidade",
+};  
